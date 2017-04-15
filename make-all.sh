b50441edd554c9264461ad16872ad6698d1c2908
slush0 <slush@satoshilabs.com>
2017-04-15 20:56:29 +0200

    Helper script to build all packages

---
+++ make-all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+cd `dirname $0`
+
+cd btc && make && mv *.deb ../ && cd ..
+cd dash && make && mv *.deb ../ && cd ..
+cd ltc && make && mv *.deb ../ && cd ..
+cd regtest && make && mv *.deb ../ && cd ..
+cd testnet && make && mv *.deb ../ && cd ..
