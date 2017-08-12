c7649b71dabcbbed49a731ddf37965d23b66fb1f
slush <slush@satoshilabs.com>
2017-08-12 15:26:39 +0200

    Added robots.txt (deny all) for all packages.

--- make-all.sh
+++ make-all.sh
@@ -4,7 +4,10 @@ set -e
 cd `dirname $0`
 
 cd btc && make && mv *.deb ../ && cd ..
+cd bcc && make && mv *.deb ../ && cd ..
 cd dash && make && mv *.deb ../ && cd ..
 cd ltc && make && mv *.deb ../ && cd ..
 cd regtest && make && mv *.deb ../ && cd ..
 cd testnet && make && mv *.deb ../ && cd ..
+cd zec && make && mv *.deb ../ && cd ..
+
